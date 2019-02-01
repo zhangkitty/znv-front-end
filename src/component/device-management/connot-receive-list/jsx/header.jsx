cfeb72a7cabf1705e3dbce7431b3a1ad66294e5f
peilizhi <907707850@qq.com>
2019-02-01 15:34:43 +0800

    样式更改

--- src/component/device-management/connot-receive-list/jsx/header.jsx
+++ src/component/device-management/connot-receive-list/jsx/header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const tmp = (props) => {
   console.log(1);
   return (
-    <a download href="/rqs-dftc/exception/downloaddevicelist?checkStatus=2">下载</a>
+    <a download href="/rqs-dftc/exception/downloaddevicelist?checkStatus=2"><input type="button" value="下载" onMouseOver="this.style.backgroundPosition='left -45px'" onMouseOut="this.style.backgroundPosition='left top'" /></a>
   );
 };
 
