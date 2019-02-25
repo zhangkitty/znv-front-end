2d57bba6f2f7a1dc28b3a73e3602262c26c35ab0
zhangcaochao <zhangcaochao30@gmail.com>
2019-02-25 11:47:01 +0800

    小bug

--- src/component/system-management/user-management/server.js
+++ src/component/system-management/user-management/server.js
@@ -1,13 +1,13 @@
 import { request } from 'utils/index';
 
-export const getOrgTreeSer = (props) => request({
-  url: `/srm/org/query/tree`,
-  method: `get`,
+export const getOrgTreeSer = props => request({
+  url: '/srm/org/query/tree',
+  method: 'get',
 });
 
-export const addOrgSer = (props) => request({
-  url: `/srm/org/add`,
-  method: `post`,
+export const addOrgSer = props => request({
+  url: '/srm/org/add',
+  method: 'post',
   data: {
     topOrgId: props.parentOrgId.indexOf('.') !== -1 ? props.parentOrgId.split('.')[1] : props.topOrgId,
     parentOrgId: props.parentOrgId.indexOf('.') !== -1 ? props.parentOrgId.split('.')[0] : props.parentOrgId,
@@ -15,18 +15,18 @@ export const addOrgSer = (props) => request({
   },
 });
 
-export const editOrgSer = (props) => request({
-  url: `/srm/org/update`,
-  method: `post`,
+export const editOrgSer = props => request({
+  url: '/srm/org/update',
+  method: 'post',
   data: {
     orgId: props.clickedId.split('.')[0],
     orgName: props.orgName,
   },
 });
 
-export const deleteOrgSer = (props) => request({
-  url: `/srm/org/delete`,
-  method: `post`,
+export const deleteOrgSer = props => request({
+  url: '/srm/org/delete',
+  method: 'post',
   data: {
     orgId: props.clickedId.split('.')[0],
   },
@@ -37,8 +37,8 @@ export const getUsersSer = (props, orgId, topOrgId) => {
     pageSize: props.pageSize,
     pageNo: props.page,
     token: props.token,
-    orgId: orgId,
-    topOrgId: topOrgId,
+    orgId,
+    topOrgId,
     fullName: props.queryUserFormData.fullName,
     phone: props.queryUserFormData.phone,
     status: props.queryUserFormData.status,
@@ -57,12 +57,12 @@ export const getUsersSer = (props, orgId, topOrgId) => {
   }).then(res => res);
 };
 
-export const addUserSer = (props) => request({
-  url: `/srm/user/add`,
-  method: `post`,
+export const addUserSer = props => request({
+  url: '/srm/user/add',
+  method: 'post',
   data: {
-    topOrgId: props.topOrgId,
-    orgId: props.orgId,
+    topOrgId: props.clickedId.split('.')[1],
+    orgId: props.clickedId.split('.')[0],
     userName: props.userName,
     fullName: props.fullName,
     phone: props.phone,
@@ -71,9 +71,9 @@ export const addUserSer = (props) => request({
   },
 });
 
-export const editUserSer = (props) => request({
-  url: `/srm/user/update`,
-  method: `post`,
+export const editUserSer = props => request({
+  url: '/srm/user/update',
+  method: 'post',
   data: {
     userId: props.userId,
     userName: props.userName,
@@ -87,38 +87,40 @@ export const editUserSer = (props) => request({
 });
 
 export const deleteUserSer = ({ props, userId }) => request({
-  url: `/srm/user/delete`,
-  method: `post`,
+  url: '/srm/user/delete',
+  method: 'post',
   data: {
-    userId: userId,
+    userId,
   },
 });
 
-export const chgUserStatusSer = ({ props, userId, status, topOrgId }) => request({
-  url: `/srm/user/disable`,
-  method: `post`,
+export const chgUserStatusSer = ({
+  props, userId, status, topOrgId,
+}) => request({
+  url: '/srm/user/disable',
+  method: 'post',
   data: {
-    userId: userId,
-    status: status,
-    topOrgId: topOrgId,
+    userId,
+    status,
+    topOrgId,
   },
 });
 
 export const getUserDetailSer = (props, userId) => request({
   url: `/srm/user/query/detail?userId=${userId}`,
-  method: `get`,
+  method: 'get',
 });
 
-export const resetPwdSer = (props) => request({
-  url: `/srm/user/pwd/reset`,
-  method: `post`,
+export const resetPwdSer = props => request({
+  url: '/srm/user/pwd/reset',
+  method: 'post',
   data: {
     userId: props.userId,
     phone: props.phone,
-  }
+  },
 });
 
-export const getRoleTreeSer = (topOrgId) => request({
+export const getRoleTreeSer = topOrgId => request({
   url: `/srm/role/query/tree?topOrgId=${topOrgId}`,
-  method: `get`,
+  method: 'get',
 });
